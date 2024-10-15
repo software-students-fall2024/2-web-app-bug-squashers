1b22b693ace8a7d8efb0b1ae62bdf3a6b1756734
ethanhcheng <ehc7678@nyu.edu>
2024-10-15 09:35:56 -0400

    added connection to mongoDB database

--- app/app.py
+++ app/app.py
@@ -1,9 +1,27 @@
 import os
 from flask import Flask, render_template, make_response, request, redirect, url_for
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
 
 def create_app():
 
     app = Flask(__name__)
+
+    MONGO_URI = os.getenv("MONGO_URI")
+
+    if MONGO_URI is None:
+        raise ValueError("Error with URI")
+    
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client.get_database("Planner")
+        collection = db["entries"]
+        print("Connected")
+    except Exception as e:
+        print(f"Error: {e}")
 
     @app.route("/")
     def home():
@@ -12,7 +30,7 @@ def create_app():
         return response
     
 
-    @app.route("/add")
+    @app.route("/add", methods = ["POST"])
     def add():
         return redirect(url_for("home"))
     
